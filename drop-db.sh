fdcca026952db2b3b6766cdbd0ae68cd6ab9bf98
salma saad <70957418+salmasaad2000@users.noreply.github.com>
2024-12-17 13:32:49 +0200

    Update drop-db.sh add GUI

--- drop-db.sh
+++ drop-db.sh
@@ -4,10 +4,12 @@ while true; do
   read -p "Enter database name: " dropDB
 
   if [ -d "$dropDB" ]; then
-    rmdir "$name2"
+    whiptail --title "Drop Databse Message" --msgbox "You deleted $dropName database secessfully" 8 45
+    rmdir "$dropDB"
     echo "You deleted the database."
     break
   else
+    whiptail --title "Drop Databse Message" --msgbox "Error to delete database $dropName" 8 45
     echo "Directory '$dropDB' does not exist."
     echo "Please enter a different name."
   fi
