f9add5e2d1dd0f7109354ed7387ad7ba8a72a050
salma saad <70957418+salmasaad2000@users.noreply.github.com>
2024-12-17 13:33:40 +0200

    Update connect-db.sh GUI

--- connect-db.sh
+++ connect-db.sh
@@ -6,6 +6,8 @@ if [ ! -d "$database_name" ]; then
                 read -p "Enter name of database: " database_name
             else
                 cd "$database_name"
+                whiptail --title "Error Message" --msgbox "Connection failed" 8 45
+       		      . ./main.sh	
                 echo "Connected to database: $PWD"
                 echo "Choose an option: "
-fi                
+fi                                
