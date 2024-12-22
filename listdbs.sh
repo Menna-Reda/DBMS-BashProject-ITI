4a2e2b397b1577200ec1c52cfdfac3a2dae7b842
Menna Reda <menna.r712@gmail.com>
2024-12-22 16:40:30 +0200

    listddbbsss

--- listdbs.sh
+++ listdbs.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 DB_PATH="$SCRIPT_DIR/DataBases"
+echo "DB_PATH is: $DB_PATH"
+ls -l "$DB_PATH"
 
 function listDBs() {
  
-    DatabasesNo=$( ls -d "$DB_PATH/*" |wc -l )
-    DatabasesList=$( ls -d "$DB_PATH/*" | cut -d/ -f9)
-    menu_height=$(($DatabasesNo+8)) 
-
+    DatabasesNo=$( ls -d $DB_PATH/* |wc -l )
+    DatabasesList=$( ls -d $DB_PATH/* | cut -d/ -f9)
+    menu_height=$(($DatabasesNo+8)) # Add padding for borders
+    echo "menu_height: $menu_height"
+    # Minimum height to ensure proper display
     if [ "$menu_height" -lt 10 ]; then
     menu_height=10
     fi
-     whiptail --title "List of DataBases" --msgbox "Number of databases: $DatabasesNo\n$DatabasesList" 15 40
+     whiptail --title "List of DataBases" --msgbox "Number of databases: $DatabasesNo\n$DatabasesList" $menu_height 40
 }
 listDBs
