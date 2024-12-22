43257a9674c7f910b58de374541c84d8340d9f5e
Menna Reda <menna.r712@gmail.com>
2024-12-22 10:25:54 +0200

    editting designs

--- listtbs.sh
+++ listtbs.sh
@@ -3,8 +3,14 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 DB_PATH="$SCRIPT_DIR/DataBases"
 function listTBs(){
     dbName=$1
-    DatabasesNo=$( ls  $DB_PATH/$dbName/* |wc -l )
+    TablesNo=$( ls  $DB_PATH/$dbName/* |wc -l )
     DatabasesList=$( ls  $DB_PATH/$dbName/* | cut -d/ -f10)
-    whiptail --title "List of tables in" --msgbox "Number of Tables: $DatabasesNo\n$DatabasesList" 10 40
+    menu_height=$(($TablesNo*2)) # Add padding for borders
+    echo "menu_height: $menu_height"
+    # Minimum height to ensure proper display
+    if [ "$menu_height" -lt 10 ]; then
+    menu_height=10
+    fi
+    whiptail --title "List of tables in" --msgbox "Number of Tables: $TablesNo\n$DatabasesList" $menu_height 40
 }
 listTBs $1
