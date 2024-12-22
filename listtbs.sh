1edc6b190e2ac154cc4dc0ddc0c87c50b2eb52a8
Menna Reda <menna.r712@gmail.com>
2024-12-22 09:47:35 +0200

    listtables edit

--- listtbs.sh
+++ listtbs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-DB_PATH="$SCRIPT_DIR/DataBase"
+DB_PATH="$SCRIPT_DIR/DataBases"
 function listTBs(){
-    DatabasesNo=$( ls -f $DB_PATH/$dbName/* |wc -l )
-    DatabasesList=$( ls -f $DB_PATH/$dbName/* )
+    dbName=$1
+    DatabasesNo=$( ls  $DB_PATH/$dbName/* |wc -l )
+    DatabasesList=$( ls  $DB_PATH/$dbName/* | cut -d/ -f10)
     whiptail --title "List of tables in" --msgbox "Number of Tables: $DatabasesNo\n$DatabasesList" 10 40
 }
-listTBs
+listTBs $1
